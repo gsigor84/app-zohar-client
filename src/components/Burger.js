7919d4ddd210d84730ebec7f60fc3a2ab0bd6d57
igor silva <gsigor84@gmail.com>
2021-03-26 13:10:23 +0000

    asdfasd

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,35 +1,48 @@
 import React, {useState}from 'react'
 import {Button} from "antd";
 import {MenuUnfoldOutlined} from "@ant-design/icons";
+import {Link} from 'react-router-dom'
+import {useTransition, animated} from 'react-spring'
 
 const state = {
     size: 'large',
 };
 
-
-
 function Burger () {
+
 const[status,setStatus]=useState(false);
+
+    const [items] = useState([''])
+    const transitions = useTransition(items, item => item.key, {
+        from: { transform: 'translate3d(0,-40px,0)' },
+        enter: { transform: 'translate3d(0,0px,0)' },
+        leave: { transform: 'translate3d(0,-40px,0)' },
+    })
 
 let menu
 
     if(status){
         menu=(
-            <nav className={'nav-menu'}>
+            transitions.map(({ item, props, key }) =>
+                <animated.div key={key} style={props}>
+
+            <nav className={'nav-menu'} onClick={()=> setStatus(false)}>
                 <ul className={'nav-item'}>
-                    <li className={'nav-link'}>Home</li>
-                    <li className={'nav-link'}>USA Trends</li>
-                    <li className={'nav-link'}>USA Tech</li>
-                    <li className={'nav-link'}>UK Trends</li>
-                    <li className={'nav-link'}>UK Tech</li>
-                    <li className={'nav-link'}>Germany Trends</li>
-                    <li className={'nav-link'}>Germany Tech</li>
-                    <li className={'nav-link'}>Indian Trends</li>
-                    <li className={'nav-link'}>Indian Tech</li>
-                    <li className={'nav-link'}>About</li>
+                    <Link  to={'/'} className={'nav-link'}>Home</Link>
+                    <Link  to={'/ustrends'} className={'nav-link'}>USA Trends</Link>
+                    <Link  to={'/ustech'} className={'nav-link'}>USA Tech</Link>
+                    <Link  to={'/uktrends'} className={'nav-link'}>UK Trends</Link>
+                    <Link  to={'/uktech'} className={'nav-link'}>UK Tech</Link>
+                    <Link  to={'/detrends'} className={'nav-link'}>Germany Trends</Link>
+                    <Link  to={'/detech'} className={'nav-link'}>Germany Tech</Link>
+                    <Link  to={'/intrends'} className={'nav-link'}>Indian Trends</Link>
+                    <Link  to={'/intech'} className={'nav-link'}>Indian Tech</Link>
+                    <Link  to={'/about'} className={'nav-link'}>About</Link>
 
                 </ul>
             </nav>
+                    {item.text}  </animated.div>
+        )
         )
 
     }
